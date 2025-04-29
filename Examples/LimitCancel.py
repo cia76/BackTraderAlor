50eef101db2df03163142bdde88c48cf4175b609
cia76 <ichechet@mail.ru>
2025-04-29 21:46:27 +0500

    Перенос библиотеки расписания

--- Examples/LimitCancel.py
+++ Examples/LimitCancel.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import backtrader as bt
 
 from BackTraderAlor.ALStore import ALStore  # Хранилище Alor
-from MarketPy.MOEX import Stocks, Futures  # Расписания торгов фондового/срочного рынков Московской Биржи
+from FinLabPy.Schedule.MOEX import Stocks, Futures  # Расписания торгов фондового/срочного рынков Московской Биржи
 
 
 # noinspection PyShadowingNames,PyProtectedMember
